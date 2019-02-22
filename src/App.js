e3d6e51aa775587ef4269a4300cdbdca61e26ee7
aditisobti <sobti_aditi@yahoo.com>
2019-02-22 15:25:14 -0800

    minor update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div>Vineet Batta</div>
+          <div>Aditi Sobti</div>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
